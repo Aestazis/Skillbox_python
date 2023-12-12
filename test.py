58ec111231889cf89ca20f712f27c2bc16449f2b
Aestazis <intrelogic@mail.ru>
2023-12-12 19:24:02 +0300

    favorite_movies и test

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
-print('test')
-print('test2')
+# тест коммитов
+#print('test')
+#print('test2')
 
-print('jopa')
+#print('jopa')
 
-# тест значений
 
 print(4 > 1)
+# выводится True или False
+
+print("""jopa\npizda""")
+# выводятся две строки. 1 jopa. 2 pizda
+
+kabachok = "шляпа, вторая шляпа, третья шляпа"
+print(kabachok[:5])
+print(kabachok[7:19])
+#выводятся последовательно символы из кабачка
+
+
+
+
+
+
 
 
