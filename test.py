ea8e1cd994afad16328fbfc0f9ff9e8cd0df6b32
Aestazis <intrelogic@mail.ru>
2023-12-12 21:32:01 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 # тест коммитов
 #print('test')
 #print('test2')
-
 #print('jopa')
-
 
 print(4 > 1)
 # выводится True или False
@@ -14,7 +12,28 @@ print("""jopa\npizda""")
 kabachok = "шляпа, вторая шляпа, третья шляпа"
 print(kabachok[:5])
 print(kabachok[7:19])
-#выводятся последовательно символы из кабачка
+# выводятся последовательно символы из кабачка
+
+# int - целое число
+# float - число с плавающей точкой
+# str - строка
+batat = 150
+tvorog = 150.4
+pirozhok = 'хлебобулочное изделие'
+print(type(batat))
+print(type(tvorog))
+print(type(pirozhok))
+# выводится тип переменной
+
+print(isinstance(batat, int))
+# выводится True или False в зависимости от типа переменной
+
+
+
+
+
+
+
 
 
 
