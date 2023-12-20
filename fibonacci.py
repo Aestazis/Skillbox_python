024f8fb86a0ae57570f45b926f30b303087569e4
Aestazis <intrelogic@mail.ru>
2023-12-20 18:50:03 +0300

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+f1, f2 = 1, 1
+i = int(input('Введи до какого числа вывести последовательность: '))
+print('0')
+print(f1)
+print(f1)
+
+while f2 < i:
+    next_1 = f2
+    next_2 = next_1 + f1
+    f1 = next_1
+    f2 = next_2
+    print(f2)
+
+
+
+
+
+
+
+
+
+
+
+
