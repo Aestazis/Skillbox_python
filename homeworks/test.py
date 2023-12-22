835884ae0c31f698c25fb98479097a2ef2337152
Aestazis <intrelogic@mail.ru>
2023-12-22 17:20:06 +0300

    test and note

--- homeworks/test.py
+++ homeworks/test.py
@@ -1,14 +1,12 @@
-my_pets = ['dog', 'lion', 'skunk', 'hamster', 'cat', 'monkey']
-i = -1
-while i < len(my_pets):
-    i = i + 1
-    if i == 2:
-        continue
-    pet = my_pets[i]
-    print('вытащили', pet)
-    if pet == 'cat':
-        print('это кот')
+my_pets = ['lion', 'cat', 'dog', 'horse', 'elephant']
+for pets in my_pets:
+    print('берем', pets)
+    if pets == 'dog':
+        print('мы нашли собаку, всё хорошо')
         break
+    print('нет')
 else:
-    print('не нашли...')
-print('конец')
+    print('собака в списке не найдена')
+
+
+
